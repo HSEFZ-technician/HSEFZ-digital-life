2b15dc26cc90a4a5f7017b79ee130953597b05b7
CodeIce <98366659+Code1ce@users.noreply.github.com>
2023-11-10 23:06:14 +0800

    Match Map

--- league/urls.py
+++ league/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 app_name = 'league'
 urlpatterns = [
     path('', views.index),
+    path('match_map', views.match_map,  name='match_map'),
 ]
--- league/views.py
+++ league/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
+# @login_required()
 def index(request):
     return render(request, 'league/home.html')
+
+def match_map(request):
+    return render(request, 'league/match_map.html', {'title': 'EFZ体育联赛·赛程图'})
