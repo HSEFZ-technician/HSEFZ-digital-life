88b4ab6dddefb2acb99bce73ed5c283e1fe5585a
Louis Deng <louisaeilotd@gmail.com>
2025-09-24 16:27:59 +0800

    Update views.py

--- verification/views.py
+++ verification/views.py
@@ -3,28 +3,28 @@ from django.db import connection
 import re
 
 def verify_student(request: HttpRequest):
-    student_id = request.GET.get('id', None)
+    mail = request.GET.get('mail', None)
 
-    if not student_id:
-        return JsonResponse({'error': '缺少 "id" 参数'}, status=400)
+    if not mail:
+        return JsonResponse({'error': '缺少 "mail" 参数'}, status=400)
 
-    if not re.match(r'^\d{9}$', student_id):
-        return JsonResponse({'error': '无效的 "id" 格式，必须是9位数字'}, status=400)
+    # if not re.match(r'^\d{9}$', mail):
+    #     return JsonResponse({'error': '无效的 "mail" 格式，必须是9位数字'}, status=400)
 
     try:
         with connection.cursor() as cursor:
             
-            query = "SELECT student_real_name, email FROM club_studentclubdata WHERE student_id = %s"
+            query = "SELECT student_real_name, student_id FROM club_studentclubdata WHERE username=\"%s\";"
             
-            cursor.execute(query, (student_id,))
+            cursor.execute(query%mail)
             
             result = cursor.fetchone()
 
             if result:
-                name, email = result
+                name, id = result
                 data = {
                     'name': name,
-                    'email': email,
+                    'id': id,
                 }
                 return JsonResponse(data, status=200)
             else:
